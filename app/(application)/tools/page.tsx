b200b515b88ba0f57ff0b7436cfd4c08784324c1
Ahmed0Idriss0dev <webocamw224@gmail.com>
2025-06-10 11:30:12 +0100

    delpoy changes 2

--- app/(application)/tools/page.tsx
+++ app/(application)/tools/page.tsx
@@ -14,9 +14,9 @@ const page = async() => {
         </div>
 
       <div className="border border-treand-950/30 p-2.5 rounded-2xl h-36 w-1/2">
-      <textarea name="" className='w-full h-[67%] resize-none outline-0 p-2.5' id=""></textarea>
+      <textarea name="" placeholder='Whats Next? ' className='w-full h-[67%] resize-none outline-0 p-2.5' id=""></textarea>
       <div className="w-full gap-1 flex justify-end items-center">
-      <button className='button bg-treand-500 text-black w-26 rounded-2xl flex justify-center items-center ' >
+      <button className='button duration-200 hover:bg-amber-300/20  text-black w-11  rounded-2xl flex justify-center items-center ' >
         <Send/>
       </button>
       </div>
