44dda67c7ee5ec24ffce4dfd8dc5880028288a52
Ahmed0Idriss0dev <webocamw224@gmail.com>
2025-06-10 10:43:43 +0100

    fix more errors

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-    allowedDevOrigins: ['*'],
+    allowedDevOrigins: ['treandsniper.vercel.app'],
 };
 
 export default nextConfig;
